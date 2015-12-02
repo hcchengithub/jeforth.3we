1be53dc817987cb8f67f9e1bc0cc2f4a61458cea
H.C. Chen <hcchen5600@gmail.com>
2015-12-03 00:21:04 +0800

    save
    working on git.f article section 5.5

--- f/jeforth.f
+++ f/jeforth.f
@@ -864,7 +864,12 @@ code abort      reset() end-code // ( -- ) Reset the forth system.
 
 code literal 	( n -- ) \ Compile TOS as an anonymous constant
 				var literal = pop();
-				var getLiteral = eval("var f;f=function(){push(literal)/*(" + mytypeof(literal) + ")" + literal.toString() + " */}");
+				var getLiteral = eval(
+						"var f;f=function(){push(literal)/*(" 
+						+ mytypeof(literal) + ")" 
+						+ literal.toString().slice(0,20).replace(/\*[/]/,"*_/") // avoid "*/" and longer string
+						+ " */}"
+					);
 				comma(getLiteral);
 				end-code
 code alias      ( Word <alias> -- ) \ Create a new name for an existing word
