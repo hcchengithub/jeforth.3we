b77fbe2368eb867999a5169e79726125950a13d4
H.C. Chen <hcchen5600@gmail.com>
2016-05-03 18:32:40 +0800

    SimpleMDE works on 3HTM
    with problems

--- 3nd.bat
+++ 3nd.bat
@@ -1,3 +1,6 @@
-
+if a%COMPUTERNAME%==aWKS-38EN3476     set NODEJSHOME=C:\Program Files\nodejs
+if a%COMPUTERNAME%==aWKS-4AEN0404     set NODEJSHOME=C:\Program Files\nodejs
+set NODE_PATH=%NODEJSHOME%\node_modules
 @REM set NODE_PATH=%NODEJSHOME%\node_modules
+
 node jeforth.3nd.js %1 %2 %3 %4 %5 %6 %7 %8 %9
--- 3nw.bat
+++ 3nw.bat
@@ -3,6 +3,9 @@
 @REM chrome.exe http://127.0.0.1:9222
 @REM nw --remote-debugging-port=9222 ../jeforth.3we
 
+if a%COMPUTERNAME%==aWKS-38EN3476     set NODEJSHOME=C:\Program Files\nodejs
+if a%COMPUTERNAME%==aWKS-4AEN0404     set NODEJSHOME=C:\Program Files\nodejs
+set NODE_PATH=%NODEJSHOME%\node_modules;%NODEJSHOME%\node_modules\simplemde\node_modules
 start nw ../jeforth.3we %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 
