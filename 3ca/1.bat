b1a37704fd15b2b4cb748e19404c9b74e4a6ebfa
H.C. Chen <hcchen5600@gmail.com>
2016-11-16 08:57:57 +0800

    working on 3ca. In the manifest.json, main.js does not work index.html does.

---
+++ 3ca/1.bat
@@ -0,0 +1,7 @@
+mklink fig.tw128x128.png c:\Users\hcche\Documents\GitHub\jeforth.3we\playground\3ca\fig.tw128x128.png
+mklink fig.tw16x16.png c:\Users\hcche\Documents\GitHub\jeforth.3we\playground\3ca\fig.tw16x16.png
+mklink jeforth-chrome-extension.html c:\Users\hcche\Documents\GitHub\jeforth.3we\playground\3ca\jeforth-chrome-extension.html
+mklink main.js c:\Users\hcche\Documents\GitHub\jeforth.3we\playground\3ca\main.js
+mklink manifest.json.Chrome-app c:\Users\hcche\Documents\GitHub\jeforth.3we\playground\3ca\manifest.json.Chrome-app
+mklink script.js c:\Users\hcche\Documents\GitHub\jeforth.3we\playground\3ca\script.js
+mklink style.css c:\Users\hcche\Documents\GitHub\jeforth.3we\playground\3ca\style.css
---
+++ 3ca/2.bat
@@ -0,0 +1,9 @@
+rem mklink fig.tw128x128.png c:\Users\hcche\Documents\GitHub\jeforth.3we\playground\3ca\fig.tw128x128.png
+
+mklink /d  3htm                c:\Users\hcche\Documents\GitHub\jeforth.3we\3htm
+mklink /d  demo                c:\Users\hcche\Documents\GitHub\jeforth.3we\demo
+mklink /d  "external modules"    "c:\Users\hcche\Documents\GitHub\jeforth.3we\external modules"
+mklink /d  f                   c:\Users\hcche\Documents\GitHub\jeforth.3we\f
+mklink /d  js                  c:\Users\hcche\Documents\GitHub\jeforth.3we\js
+mklink /d  playground          c:\Users\hcche\Documents\GitHub\jeforth.3we\playground
+mklink /d  project-k           c:\Users\hcche\Documents\GitHub\jeforth.3we\project-k
