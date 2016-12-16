669cb0d7c0e0e7cdc17db94f3b93bfd9505c1c53
H.C. Chen <hcchen5600@gmail.com>
2016-12-16 17:48:34 +0800

    a little step further

--- playground/123.f
+++ playground/123.f
@@ -8,45 +8,9 @@ source~
 \ 
 \ 2016/12/11  09:17    <DIR>          .
 \ 2016/12/11  09:17    <DIR>          ..
-\ 2016/12/11  09:17                 0 123.f -=EOF=- 
-\ 2016/12/09  23:13            13,854 12choc.f
-\ 2016/11/26  21:05            10,538 12choc.xlsx
-2016/09/12  17:08            82,676 2016-01-081.jpg
-2016/09/12  17:08    <DIR>          3wsh
-2016/11/26  09:53                16 admin.bat
-2016/09/12  17:08             3,353 anual.htm
-2016/12/09  23:13             2,577 balls.f
-2016/09/12  17:08               767 beep.f
-2016/12/09  23:13            14,284 chipmunk.log
-2016/09/12  17:08             9,834 css.html
-2016/09/12  17:08           282,988 disable jquery-1.10.2.js
-2016/09/12  17:08            84,349 disable jquery-2.1.4.min.js
-2016/09/12  17:08               663 download.js
-2016/09/12  17:08               835 echo.js
-2016/09/12  17:08             9,968 ed2.f
-2016/09/12  17:08             7,203 editor-study.html
-2016/09/12  17:08           382,864 editor.png
-2016/09/12  17:08             6,885 excel-tools.html
-2016/09/12  17:08             6,409 html5_audio_demo.f
-2016/09/12  17:08               569 htmlplayground.f
-2016/09/12  17:08            18,094 jeforth.js
-2016/09/12  17:08                30 jefortheditor.html
-2016/09/12  17:08            28,910 k16.f
-2016/09/12  17:08               303 math.f
-2016/09/12  17:08             1,426 modify.f
-2016/09/12  17:08             7,398 mylocal.json
-2016/09/12  17:08             1,847 nan.f
-2016/12/10  09:39             2,771 npp.f
-2016/09/12  17:08             7,960 playCSS.f
-2016/09/12  17:08               886 squirrel.js
-2016/09/12  17:08            36,138 suan.f
-2016/09/12  17:08             3,663 template.f
-2016/09/12  17:08             9,855 test.md
-2016/09/12  17:08               413 transpiler.f
-2016/09/12  17:08           109,843 use your own words to dictate javascript what to do.pptx
-2016/09/12  17:08             2,370 WshShell child-process.f
-2016/09/12  17:08            11,138 xjeforth.hta
-2016/09/12  17:08             8,848 xneat.f
-2016/09/12  17:08            12,071 悟透JavaScript.f
-              39 個檔案       1,184,596 位元組
-               3 個目錄  578,957,586,432 位元組可用
+\ 2016/12/11  09:17                 0 123.f 
+
+js> tib to TIB js> ntib to nTIB *debug* 99>>
+-=EOF=- 
+
+.( I am invisible because EOF has blocked me )
