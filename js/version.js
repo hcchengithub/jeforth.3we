0e374a5abee5ce7e8ff7fc4ce460068b0ea38f0b
H.C. Chen <hcchen5600@gmail.com>
2017-09-23 15:49:10 +0800

    about to release r3

--- js/version.js
+++ js/version.js
@@ -1,9 +1,10 @@
 
 // 
 // jeofrth.3we minor version
-// Use the same version code for all applications or there will be too many files to modify for a release.
+// Use the same version code for all applications or there will 
+// be too many files to modify for a release.
 //
 
 "uses strict";
-var jeforth3we_minor_version = 303; // 301~, 401~, 501~, . . .
+var jeforth3we_minor_version = 304; // 301~, 401~, 501~, . . .
 if (typeof exports!='undefined') exports.jeforth3we_minor_version = jeforth3we_minor_version; // export for node.js APP
