12668646a94a17d922fc20abd0ae738ca1bfec13
hcchengithub <hcchen5600@gmail.com>
2023-05-26 14:34:10 +0800

    add chatlkk.f to 3nw

--- 3nw.bat
+++ 3nw.bat
@@ -1,14 +1,10 @@
 cd %~dp0
 
-@REM ------ debug -------------------
-@REM chrome.exe http://127.0.0.1:9222
-@REM nw --remote-debugging-port=9222 ../jeforth.3we
-
 set NODEJSHOME=C:\Program Files\nodejs
 if a%COMPUTERNAME%==aWKS-38EN3476     set NODEJSHOME=C:\Program Files\nodejs
 if a%COMPUTERNAME%==aWKS-4AEN0404     set NODEJSHOME=C:\Program Files\nodejs
 if a%COMPUTERNAME%==aDESKTOP-Q4DUVFG  set NODEJSHOME=C:\Program Files\nodejs
 set NODE_PATH=%NODEJSHOME%\node_modules;%NODEJSHOME%\node_modules\simplemde\node_modules
-path=%path%;c:\Users\User\Downloads\nwjs-sdk-v0.50.1-win-x64
-start nw ../jeforth.3we nop %*
+path=c:\Users\hcchen\AppData\Roaming\npm\node_modules\nw\SDK;%path%
+start nw ../jeforth.3nw nop %*
 
